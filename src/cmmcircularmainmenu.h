a80cb0e95dcffc1ac45ce8c1890428a35ca1df16
cole-anstey@ntlworld.com <cole-anstey@ntlworld.com@e503a6f0-9f47-0410-a851-0334bb8926b4>
2008-03-24 09:38:29 +0000

    Refactored code and fixed a couple of errors when trying to retrieve pixbufs.

--- src/cmmcircularmainmenu.h
+++ src/cmmcircularmainmenu.h
@@ -18,8 +18,8 @@
  *
  * See http://code.google.com/p/circular-application-menu/
  */
-#ifndef __CA_CIRCULAR_APPLICATIONS_MENU_H__
-#define __CA_CIRCULAR_APPLICATIONS_MENU_H__
+#ifndef __CA_CIRCULAR_APPLICATION_MENU_H__
+#define __CA_CIRCULAR_APPLICATION_MENU_H__
 
 #include <gtk/gtk.h>
 
@@ -28,13 +28,14 @@
 
 G_BEGIN_DECLS
 
-#define CA_TYPE_CIRCULAR_APPLICATIONS_MENU          (ca_circular_applications_menu_get_type ())
-#define CA_CIRCULAR_APPLICATIONS_MENU(obj)          (G_TYPE_CHECK_INSTANCE_CAST ((obj), CA_TYPE_CIRCULAR_APPLICATIONS_MENU, CaCircularApplicationsMenu))
-#define CA_CIRCULAR_APPLICATIONS_MENU_CLASS(obj)    (G_TYPE_CHECK_CLASS_CAST ((obj), CA_CIRCULAR_APPLICATIONS_MENU, CaCircularApplicationsMenuClass))
-#define CA_IS_CIRCULAR_APPLICATIONS_MENU(obj)       (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CA_TYPE_CIRCULAR_APPLICATIONS_MENU))
-#define CA_IS_CIRCULAR_APPLICATIONS_MENU_CLASS(obj) (G_TYPE_CHECK_CLASS_TYPE ((obj), EFF_TYPE_CIRCULAR_APPLICATIONS_MENU))
-#define CA_CIRCULAR_APPLICATIONS_MENU_GET_CLASS	    (G_TYPE_INSTANCE_GET_CLASS ((obj), CA_TYPE_CIRCULAR_APPLICATIONS_MENU, CaCircularApplicationsMenuClass))
+#define CA_TYPE_CIRCULAR_APPLICATION_MENU          (ca_circular_application_menu_get_type ())
+#define CA_CIRCULAR_APPLICATION_MENU(obj)          (G_TYPE_CHECK_INSTANCE_CAST ((obj), CA_TYPE_CIRCULAR_APPLICATION_MENU, CaCircularApplicationMenu))
+#define CA_CIRCULAR_APPLICATION_MENU_CLASS(obj)    (G_TYPE_CHECK_CLASS_CAST ((obj), CA_CIRCULAR_APPLICATION_MENU, CaCircularApplicationMenuClass))
+#define CA_IS_CIRCULAR_APPLICATION_MENU(obj)       (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CA_TYPE_CIRCULAR_APPLICATION_MENU))
+#define CA_IS_CIRCULAR_APPLICATION_MENU_CLASS(obj) (G_TYPE_CHECK_CLASS_TYPE ((obj), EFF_TYPE_CIRCULAR_APPLICATION_MENU))
+#define CA_CIRCULAR_APPLICATION_MENU_GET_CLASS     (G_TYPE_INSTANCE_GET_CLASS ((obj), CA_TYPE_CIRCULAR_APPLICATION_MENU, CaCircularApplicationMenuClass))
 
+/* Leaf types. */
 typedef enum
 {
     ROOT_LEAF,
@@ -42,26 +43,28 @@ typedef enum
     SUB_FILE_LEAF
 } LeafType;
 
+/* Gylph types. */
 typedef enum
 {
-	GLYPH_UNKNOWN,
+    GLYPH_UNKNOWN,
     GLYPH_ROOT_CENTRE,
     GLYPH_FILE_MENU_CENTRE,
     GLYPH_FILE,
     GLYPH_FILE_MENU,
-	GLYPH_TAB
+    GLYPH_TAB
 } GlyphType;
 
+/* The current animation state of a leaf. */
 typedef enum
 {
     NO_STATE,
     INITIAL_OPEN
 } AnimationState;
 
-typedef struct _CaCircularApplicationsMenu        CaCircularApplicationsMenu;
-typedef struct _CaCircularApplicationsMenuClass   CaCircularApplicationsMenuClass;
+typedef struct _CaCircularApplicationMenu        CaCircularApplicationMenu;
+typedef struct _CaCircularApplicationMenuClass   CaCircularApplicationMenuClass;
 
-struct _CaCircularApplicationsMenu
+struct _CaCircularApplicationMenu
 {
     GtkDrawingArea parent;
 
@@ -70,9 +73,9 @@ struct _CaCircularApplicationsMenu
     /*< public >*/
 };
 
-struct _CaCircularApplicationsMenuClass
+struct _CaCircularApplicationMenuClass
 {
-	GtkDrawingAreaClass parent_class;
+    GtkDrawingAreaClass parent_class;
 };
 
 typedef struct _CaMenuRender CaMenuRender;
@@ -80,89 +83,93 @@ typedef struct _CaSegmentRender CaSegmentRender;
 typedef struct _CaFileItem CaFileItem;
 typedef struct _CaFileLeaf CaFileLeaf;
 
-
+/* Structure to hold calculated menu values for rendering. */
+/* Held here to stop them being calculated more than once. */
 struct _CaMenuRender
 {
-	// Render a child menu.
-	/*
-	 C   E
-	 B   F
-	A     A
-	A     A
-	A     A
-	 AAAAA
-	*/
-	gint Bcircle_x;
-	gint Bcircle_y;
-	gint Ccircle_x;
-	gint Ccircle_y;
-	gint Ecircle_x;
-	gint Ecircle_y;
-	gint Fcircle_x;
-	gint Fcircle_y;
-	gint Bcircle_arc_x;
-	gint Bcircle_arc_y;
-	gint Ccircle_arc_x;
-	gint Ccircle_arc_y;
-	gdouble lowest_angle_nearest;
-	gdouble highest_angle_nearest;
-	gdouble Bcircle_lowest_angle;
-	gdouble Bcircle_highest_angle;
-	gdouble Fcircle_lowest_angle;
-	gdouble Fcircle_highest_angle;
-	gdouble lowest_angle_farthest;
-	gdouble highest_angle_farthest;
-	gdouble Ccircle_lowest_angle;
-	gdouble Ccircle_highest_angle;
-	gdouble Ecircle_lowest_angle;
-	gdouble Ecircle_highest_angle;
+    // Render a child menu.
+    /*
+     C   E
+     B   F
+    A     A
+    A     A
+    A     A
+     AAAAA
+    */
+    gint Bcircle_x;
+    gint Bcircle_y;
+    gint Ccircle_x;
+    gint Ccircle_y;
+    gint Ecircle_x;
+    gint Ecircle_y;
+    gint Fcircle_x;
+    gint Fcircle_y;
+    gint Bcircle_arc_x;
+    gint Bcircle_arc_y;
+    gint Ccircle_arc_x;
+    gint Ccircle_arc_y;
+    gdouble lowest_angle_nearest;
+    gdouble highest_angle_nearest;
+    gdouble Bcircle_lowest_angle;
+    gdouble Bcircle_highest_angle;
+    gdouble Fcircle_lowest_angle;
+    gdouble Fcircle_highest_angle;
+    gdouble lowest_angle_farthest;
+    gdouble highest_angle_farthest;
+    gdouble Ccircle_lowest_angle;
+    gdouble Ccircle_highest_angle;
+    gdouble Ecircle_lowest_angle;
+    gdouble Ecircle_highest_angle;
 
-	gdouble tab_glyph_radius;
-	gint tab_glyph_x;
-	gint tab_glyph_y;
+    gdouble tab_glyph_radius;
+    gint tab_glyph_x;
+    gint tab_glyph_y;
 };
 
+/* Structure to hold calculated segment values for rendering. */
+/* Held here to stop them being calculated more than once.    */
 struct _CaSegmentRender
 {
-	/*
-	Segment
+    /*
+    Segment
 
-	CDC
-	BAB
-	*/
-	gint Aradius;
-	gint Bradius;
-	gint Cradius;
-	gint Dradius;
-	gdouble Afrom_angle;
-	gdouble Bfrom_angle;
-	gdouble Cfrom_angle;
-	gdouble Dfrom_angle;
-	gdouble Ato_angle;
-	gdouble Bto_angle;
-	gdouble Cto_angle;
-	gdouble Dto_angle;
-	gint B_lowest_circle_x;
-	gint B_lowest_circle_y;
-	gint C_lowest_circle_x;
-	gint C_lowest_circle_y;
-	gint B_highest_circle_x;
-	gint B_highest_circle_y;
-	gint C_highest_circle_x;
-	gint C_highest_circle_y;
+    CDC
+    BAB
+    */
+    gint Aradius;
+    gint Bradius;
+    gint Cradius;
+    gint Dradius;
+    gdouble Afrom_angle;
+    gdouble Bfrom_angle;
+    gdouble Cfrom_angle;
+    gdouble Dfrom_angle;
+    gdouble Ato_angle;
+    gdouble Bto_angle;
+    gdouble Cto_angle;
+    gdouble Dto_angle;
+    gint B_lowest_circle_x;
+    gint B_lowest_circle_y;
+    gint C_lowest_circle_x;
+    gint C_lowest_circle_y;
+    gint B_highest_circle_x;
+    gint B_highest_circle_y;
+    gint C_highest_circle_x;
+    gint C_highest_circle_y;
 
-	// Arrow.
-	gdouble arrow_percentage;
-	gdouble common_angle;
-	gint arrow_point_x;
-	gint arrow_point_y;
-	gint arrow_side_x;
-	gint arrow_side_y;
-	gint outer_arrow_radius;
-	gint inner_arrow_radius;
-	gdouble arrow_angle;
+    // Arrow.
+    gdouble arrow_percentage;
+    gdouble common_angle;
+    gint arrow_point_x;
+    gint arrow_point_y;
+    gint arrow_side_x;
+    gint arrow_side_y;
+    gint outer_arrow_radius;
+    gint inner_arrow_radius;
+    gdouble arrow_angle;
 };
 
+/* The file-item which resides on a file-leaf. */
 struct _CaFileItem
 {
     GlyphType _type;
@@ -174,7 +181,7 @@ struct _CaFileItem
     gdouble _parent_angle;
     gdouble _circular_angle_share;
     gdouble _parent_radius;
-	CaSegmentRender* _segment_render;
+    CaSegmentRender* _segment_render;
 
     /*< Drawing >*/
     gint x;
@@ -182,6 +189,7 @@ struct _CaFileItem
     gint size;
 };
 
+/* The file-leaf containing 0-* file-items. */
 struct _CaFileLeaf
 {
     LeafType _type;
@@ -192,9 +200,8 @@ struct _CaFileLeaf
     CaFileLeaf* _parent_fileleaf;
     CaFileLeaf* _child_fileleaf;
     GList* _sub_fileleaves_list;
-    gint _sub_fileleaves_list_count;
     CaFileItem* _central_glyph;
-	CaMenuRender* _menu_render;
+    CaMenuRender* _menu_render;
 
     /*< Drawing >*/
     gint x;
@@ -203,13 +210,13 @@ struct _CaFileLeaf
 };
 
 /* Construction */
-GtkWidget* ca_circular_applications_menu_new (gint width, gint height, gboolean hideindicator, gint glyphsize);
-GType ca_circular_applications_menu_get_type(void);
+GtkWidget* ca_circular_application_menu_new (gboolean hide_preview, gboolean warp_mouse, gint glyph_size);
+GType ca_circular_application_menu_get_type(void);
 
 /* Implementation */
-CaFileLeaf* ca_circular_applications_menu(GMenuTreeDirectory *root);
-CaFileLeaf* ca_circular_applications_menu_show_leaf(GMenuTreeDirectory* menutreedirectory, LeafType leaftype, CaFileItem* fileitem, gboolean disassociated);
-void ca_circular_applications_menu_close_fileleaf(CaFileLeaf* fileleaf);
+CaFileLeaf* ca_circular_application_menu(CaCircularApplicationMenu* circular_application_menu, GMenuTreeDirectory* menutreedirectory);
+CaFileLeaf* ca_circular_application_menu_show_leaf(CaCircularApplicationMenu* circular_application_menu, GMenuTreeDirectory* menutreedirectory, LeafType leaftype, CaFileItem* fileitem, gboolean disassociated);
+void ca_circular_application_menu_close_fileleaf(CaFileLeaf* fileleaf);
 
 /* Private functions. */
 
